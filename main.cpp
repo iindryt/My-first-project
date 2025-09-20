442d5cb2b4e3c7915613348d17412580aaac4d16
iindryt <indreskomantaite7@gmail.com>
2025-09-20 14:02:43 +0300

    Sukuriamas studento duomenu ivedimas ir galutinio balo skaiciavimas
    Programa iveda studento varda, pavarde, namu darbu pazymius ir egzamino pazymi; 
    Apskaiciuoja galutini rezultata pagal formule 0.4*vidurkis+0.6*egzaminas;
    Isveda studento varda, pavarde ir galutini rezultata.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main()
-{
-  cout<<"Hello world!";
-  return 0;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+struct Studentas {
+    string vard;
+    string pav;
+    vector<int> paz;
+    int egzas;
+    float rez;
+};
+
+int main (){
+    Studentas Laik;
+    int sum=0, n, m;
+
+    cout << "Iveskite varda: "; 
+    cin >> Laik.vard;
+
+    cout << "Iveskite Pavarde: "; 
+    cin >> Laik.pav;
+
+    cout << "Iveskite pazymiu skaiciu: "; 
+    cin >> n;
+
+    for (int i=0; i<n; i++){
+        cout << "Iveskite " << i+1 << " paz. is " << n << " : "; 
+        cin >> m;
+        Laik.paz.push_back(m);
+        sum += m;
+    }
+
+    cout << "Iveskite egzamina: "; 
+    cin >> Laik.egzas;
+
+    Laik.rez = Laik.egzas * 0.6 + double(sum) / double(Laik.paz.size()) * 0.4;
+
+    cout << Laik.vard << " | " << Laik.pav << " | " << Laik.rez << endl;
 }
+
