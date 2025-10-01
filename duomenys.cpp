ca7e0bb4ddfd936d5c30f75e3d0cf1d0e003cf74
iindryt <indreskomantaite7@gmail.com>
2025-10-01 19:04:43 +0300

    Sukurta duomenys.cpp

--- duomenys.cpp
+++ duomenys.cpp
@@ -1,36 +1,33 @@
 #include "duomenys.h"
-#include "funkcijos.h" // kad būtų galima naudoti random ir t.t.
+#include "funkcijos.h"
 #include <fstream>
-#include <iostream>
 #include <sstream>
-#include <algorithm>
+#include <iostream>
 
-using namespace std;
-
-vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
-    vector<Studentas> studentai;
-    ifstream in(failoVardas);
+std::vector<Studentas> nuskaitytiIsFailo(const std::string& failoVardas) {
+    std::vector<Studentas> studentai;
+    std::ifstream in(failoVardas);
 
     if (!in) {
-        cout << "Klaida: nepavyko atidaryti failo: " << failoVardas << endl;
+        std::cerr << "Klaida: nepavyko atidaryti failo: " << failoVardas << std::endl;
         return studentai;
     }
 
-    string eilute;
-    getline(in, eilute); // praleidziam header
+    std::string eilute;
+    getline(in, eilute); // praleidžiame antraštę
 
     while (getline(in, eilute)) {
         if (eilute.empty()) continue;
 
-        istringstream ss(eilute);
-        string vard, pav;
+        std::istringstream ss(eilute);
+        std::string vard, pav;
         ss >> vard >> pav;
 
         Studentas s;
         s.vard = vard;
         s.pav = pav;
 
-        vector<int> laikPaz;
+        std::vector<int> laikPaz;
         int paz;
 
         while (ss >> paz) {
@@ -38,7 +35,7 @@ vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
         }
 
         if (laikPaz.size() < 2) {
-            cerr << "Klaida: per mazai pazymiu eiluteje: " << eilute << endl;
+            std::cerr << "Klaida: per mazai pazymiu eiluteje: " << eilute << std::endl;
             continue;
         }
 
@@ -46,11 +43,8 @@ vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
         laikPaz.pop_back();
         s.paz = laikPaz;
 
-        float vidurkis = Vidurkis(s.paz);
-        float mediana = Mediana(s.paz);
-
-        s.rezVid = s.egzas * 0.6f + vidurkis * 0.4f;
-        s.rezMed = s.egzas * 0.6f + mediana * 0.4f;
+        s.rezVid = s.egzas * 0.6f + Vidurkis(s.paz) * 0.4f;
+        s.rezMed = s.egzas * 0.6f + Mediana(s.paz) * 0.4f;
 
         studentai.push_back(s);
     }
@@ -58,10 +52,10 @@ vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
     return studentai;
 }
 
-void generuotiFaila(int kiekis, const string& failoVardas, int ndKiekis) {
-    ofstream out(failoVardas);
+void generuotiFaila(int kiekis, const std::string& failoVardas, int ndKiekis) {
+    std::ofstream out(failoVardas);
     if (!out) {
-        cerr << "Nepavyko sukurti failo: " << failoVardas << endl;
+        std::cerr << "Nepavyko sukurti failo: " << failoVardas << std::endl;
         return;
     }
 
@@ -69,16 +63,15 @@ void generuotiFaila(int kiekis, const string& failoVardas, int ndKiekis) {
     for (int i = 1; i <= ndKiekis; i++) {
         out << " ND" << i;
     }
-    out << " Egzaminas" << endl;
+    out << " Egzaminas\n";
 
     for (int i = 1; i <= kiekis; ++i) {
         out << "Vardas" << i << " Pavarde" << i;
         for (int j = 0; j < ndKiekis; ++j) {
             out << " " << random(1, 10);
         }
-        out << " " << random(1, 10) << endl;
+        out << " " << random(1, 10) << "\n";
     }
-
     out.close();
-    cout << "Failas '" << failoVardas << "' sukurtas su " << kiekis << " irasu." << endl;
+    std::cout << "Failas '" << failoVardas << "' sukurtas su " << kiekis << " irasu." << std::endl;
 }
