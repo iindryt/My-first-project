924fc2c18240faf61b77e7495e89ee6549780c0c
iindryt <indreskomantaite7@gmail.com>
2025-10-01 16:37:56 +0300

    Sukurta studentas.h

---
+++ studentas.h
@@ -0,0 +1,17 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+    std::string vard;
+    std::string pav;
+    std::vector<int> paz;
+    int egzas;
+    float rezVid;
+    float rezMed;
+};
+
+#endif // STUDENTAS_H
+
