bb7a199169827555722e972e9b1be4da775e73c3
iindryt <indreskomantaite7@gmail.com>
2025-10-16 08:35:25 +0300

    Panaikinta rusiavimas, prideta setprecision

--- testavimas.h
+++ testavimas.h
@@ -4,31 +4,25 @@
 #include <iostream>
 #include <vector>
 #include <list>
-#include <algorithm>
 #include <type_traits>
 #include <fstream>
 #include "studentas.h"
 #include "duomenys.h"
 #include "laikmatis.h"
 
+// Testavimo funkcija
 template <typename Container>
 void testuotiKonteineri(const std::string& failoVardas, const std::string& pavadinimas) {
     std::cout << "\n=== Testuojamas konteineris: " << pavadinimas << " ===\n";
 
-    Laikmatis visoLaiko;
-    auto studentai = nuskaitytiIsFailoTemplate<Container>(failoVardas);
+    Laikmatis tViso; // Bendras laikas
 
-    // --- Rusiavimas ---
-    Laikmatis tRusiavimui;
-    if constexpr (std::is_same_v<Container, std::list<Studentas>>) {
-        studentai.sort([](const Studentas& a, const Studentas& b) { return a.vard < b.vard; });
-    }
-    else {
-        std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) { return a.vard < b.vard; });
-    }
-    std::cout << "Rusiavimas truko: " << tRusiavimui.praejes_laikas() << " s.\n";
+    // --- Nuskaitymas ---
+    Laikmatis tNuskaitymui;
+    Container studentai = nuskaitytiIsFailoTemplate<Container>(failoVardas, false); // nespauzdinti viduje
+    std::cout << "Failas nuskaitytas per " << tNuskaitymui.praejes_laikas() << " s.\n";
 
-    // --- Skirstymas i kietiakiai / vargsiukai ---
+    // --- Skirstymas i kietakius / vargsiukus ---
     Laikmatis tSkirstymui;
     Container kietiakiai, vargsiukai;
     for (auto& s : studentai) {
@@ -37,31 +31,33 @@ void testuotiKonteineri(const std::string& failoVardas, const std::string& pavad
         else
             vargsiukai.push_back(s);
     }
+    std::cout << std::fixed << std::setprecision(6);
     std::cout << "Skirstymas truko: " << tSkirstymui.praejes_laikas() << " s.\n";
 
     // --- Irasymas i failus ---
     Laikmatis tFailams;
     {
         std::ofstream outKiet("kietiakiai.txt");
-        if (!outKiet) std::cerr << "Klaida: nepavyko sukurti 'kietiakiai.txt'.\n";
+        if (!outKiet)
+            std::cerr << "Klaida: nepavyko sukurti 'kietiakiai.txt'.\n";
         else {
             for (auto& s : kietiakiai)
                 outKiet << s.vard << " " << s.pav << " " << s.rezVid << "\n";
-            outKiet.close();
         }
 
         std::ofstream outVarg("vargsiukai.txt");
-        if (!outVarg) std::cerr << "Klaida: nepavyko sukurti 'vargsiukai.txt'.\n";
+        if (!outVarg)
+            std::cerr << "Klaida: nepavyko sukurti 'vargsiukai.txt'.\n";
         else {
             for (auto& s : vargsiukai)
                 outVarg << s.vard << " " << s.pav << " " << s.rezVid << "\n";
-            outVarg.close();
         }
     }
     std::cout << "Irasymas i failus truko: " << tFailams.praejes_laikas() << " s.\n";
 
+    // --- Bendras laikas ---
     std::cout << "Visas testas (" << pavadinimas << ") truko: "
-        << visoLaiko.praejes_laikas() << " s.\n";
+        << tViso.praejes_laikas() << " s.\n";
 }
 
 #endif // TESTAVIMAS_H
